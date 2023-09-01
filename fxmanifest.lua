5bdb9aee147e14ace3a88e1b1d6f22829087042f
binaryc0ding <60327351+binaryc0ding@users.noreply.github.com>
2023-08-31 20:07:22 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author '!ViDu'
-description 'Simple QB-KFC job for your FiveM Server By !ViDu'
-version '1.0.0'
+description 'Simple QB-KFC job for your FiveM Server By !ViDu Fixed By binaryc0ding'
+version '1.1.0'
 
 shared_scripts {
     'config.lua'
